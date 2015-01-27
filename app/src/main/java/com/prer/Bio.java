ffb57de13339e910f0e6b4c1ad6f0daa3d1adc7c
Brian Truong <bhtruong@gmail.com>
2015-01-27 07:13:13 -0800

    Added ActionBar to Bio

--- app/src/main/java/com/prer/Bio.java
+++ app/src/main/java/com/prer/Bio.java
@@ -1,9 +1,9 @@
 package com.prer;
 
 import android.content.Intent;
-import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,7 +20,7 @@ import org.json.JSONObject;
 import java.io.IOException;
 
 
-public class Bio extends Activity {
+public class Bio extends ActionBarActivity {
     TextView name;
     TextView description;
     HttpGet httpget;
