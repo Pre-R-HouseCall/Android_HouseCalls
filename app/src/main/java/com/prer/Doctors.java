7f1d938b969dac7f6bb54b182180beefa938333a
Brian Truong <bhtruong@gmail.com>
2015-01-27 06:43:51 -0800

    Merging Bio and Doctor updates

--- app/src/main/java/com/prer/Doctors.java
+++ app/src/main/java/com/prer/Doctors.java
@@ -8,18 +8,29 @@ import android.widget.ImageButton;
 
 public class Doctors extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_doctor_list);
-		
-		ImageButton form = (ImageButton) findViewById(R.id.doctor3_action);
-	    form.setOnClickListener(new View.OnClickListener() {
-    	
-	        public void onClick(View view) {
-	            Intent myIntent = new Intent(view.getContext(), Waitlist.class);
-	            startActivityForResult(myIntent, 0);
-	        }
-	    });
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_list);
+
+        ImageButton form = (ImageButton) findViewById(R.id.doctor3_action);
+        ImageButton bio = (ImageButton) findViewById(R.id.doctor3_profile);
+
+        form.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Login.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+        bio.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Bio.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+    }
 }
